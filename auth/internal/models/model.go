dea966c52fe62d257754fe0779989b5bd478bc54
Enyoku <mondex.2003@gmail.com>
2025-02-13 19:07:37 +0300

    Added UserProfileInfo in models folder

--- auth/internal/models/model.go
+++ auth/internal/models/model.go
@@ -3,11 +3,20 @@ package models
 import "time"
 
 type User struct {
-	Email      string `json:"email" binding:"required,email"`
-	Username   string `json:"username" binding:"required"`
-	Password   string `json:"password" binding:"required"`
-	BIO        string
-	Picture    string
-	CreatedAt  time.Time
-	LastOnline time.Time
+	Email      string    `json:"email" binding:"required,email"`
+	Username   string    `json:"username" binding:"required"`
+	Password   string    `json:"password" binding:"required"`
+	BIO        string    `json:"bio,omitempty"`
+	Picture    string    `json:"pic,omitempty"`
+	CreatedAt  time.Time `json:"created_at,omitempty"`
+	LastOnline time.Time `json:"last_online,omitempty"`
+}
+
+type UserProfileInfo struct {
+	Email      string    `json:"email"`
+	Username   string    `json:"username"`
+	BIO        string    `json:"bio,omitempty"`
+	Picture    string    `json:"pic,omitempty"`
+	CreatedAt  time.Time `json:"created_at,omitempty"`
+	LastOnline time.Time `json:"last_online,omitempty"`
 }
